12bc7a7c1d85205dbaa436544880f461620254c6
Randy Gohmann <RandyGohmann@Randys-MacBook-Air.local>
2019-11-21 14:12:49 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,15 +1,21 @@
 const cards = ["queen", "queen", "king", "king"];
 const cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped queen");
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped king");
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
-	} else {
-		alert("Sorry try again");
-	}
-}
+function checkForMatch() {
+	if (cardsInPlay.length === 2) {
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+			console.log("You found a match!");
+		} else {
+			console.log("Sorry, not a match.");
+		};
+	};
+};
+function flipCard(cardsId) {
+	console.log("User flipped " + cards[cardsId]);
+	cardsInPlay.push(cards[cardsId]);
+	checkForMatch();
+};
+flipCard(0);
+flipCard(2);
+
+
+
